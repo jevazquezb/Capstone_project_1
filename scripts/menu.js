d17b80f67971f9cbea5052d21b6941a974ccbae4
Jesus Vazquez <je.vazquezb@gmail.com>
2022-04-13 12:11:16 -0500

    Add a dynamic menu to the mobile version

---
+++ scripts/menu.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector('.menu-btn');
+const dynMenu = document.querySelector('.dynamic-menu');
+const menuItems = document.querySelectorAll('.dynamic-menu > *');
+
+function displayMenu() {
+  dynMenu.style.right = '0%';
+}
+
+
+function hideMenu(item) {
+  item.addEventListener('click', () => {
+    dynMenu.style.right = '100%';
+  });
+}
+
+menuBtn.addEventListener('click', displayMenu);
+menuItems.forEach(hideMenu);
